ee06df985d9fb02b811b0e8e30b281f834251fba
Aman <amanp70289@gmail.com>
2026-06-14 00:02:57 +0530

    Implement addComment and updateComment functions in comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,4 +1,5 @@
 import { Comment } from "../models/comment.model";
+import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 
@@ -17,11 +18,58 @@ const getVideoComment=asyncHandler(
         }
 )
 
-const addComment=async (req,res) => {
-    const {videoId}=req.params
+const addComment=asyncHandler(
+    async (req,res) => {
+    const {videoId,content}=req.body;
+    if(!content?.trim()){
+        return res
+        .status(400)
+        .json(
+            new ApiResponse(400,content,"content is required")
+        )
+    };
     
     
 }
+)
+
+const updateComment=asyncHandler(
+    async(req,res)=>{
+        const {commentId}=req.params
+        const {content}=req.body
+        if(!content?.trim())
+        {
+            return res
+            .status(400)
+            .json(
+                new ApiResponse(400,content,"Content is required")
+            )
+        }
+        const comment=await Comment.findByIdAndUpdate(
+            commentId,
+            {
+            $set: { content }
+            },
+            {
+            new: true
+            }
+        )
+
+        if(!comment)
+        {
+            throw new ApiError(404,"Comment not found")
+        }
+
+         return res.status(200).json(
+        new ApiResponse(
+            200,
+            comment,
+            "Comment updated successfully"
+        ))
+
+    } 
+
+)
 
 
 
